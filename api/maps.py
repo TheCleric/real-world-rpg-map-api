35b183f9dd47928e4b76ef80da4f3ef6a52e84d6
Adam Weeden <adam.weeden@nrccua.org>
2021-08-31 16:42:47 -0400

    Cut back a little bit

--- api/maps.py
+++ api/maps.py
@@ -16,21 +16,21 @@ def create_map(radius: int = 500) -> io.BytesIO:
         ax=axis,
         layers={
             'perimeter': {},
-            'streets': {
-                'custom_filter': '["highway"~"motorway|trunk|primary|secondary|tertiary|residential|service|unclassified|pedestrian|footway"]',  # pylint: disable=line-too-long
-                'width': {
-                    'motorway': 5,
-                    'trunk': 5,
-                    'primary': 4.5,
-                    'secondary': 4,
-                    'tertiary': 3.5,
-                    'residential': 3,
-                    'service': 2,
-                    'unclassified': 2,
-                    'pedestrian': 2,
-                    'footway': 1,
-                }
-            },
+            # 'streets': {
+            #     'custom_filter': '["highway"~"motorway|trunk|primary|secondary|tertiary|residential|service|unclassified|pedestrian|footway"]',  # pylint: disable=line-too-long
+            #     'width': {
+            #         'motorway': 5,
+            #         'trunk': 5,
+            #         'primary': 4.5,
+            #         'secondary': 4,
+            #         'tertiary': 3.5,
+            #         'residential': 3,
+            #         'service': 2,
+            #         'unclassified': 2,
+            #         'pedestrian': 2,
+            #         'footway': 1,
+            #     }
+            # },
             'building': {'tags': {'building': True, 'landuse': 'construction'}, 'union': False},
             'water': {'tags': {'natural': ['water', 'bay']}},
             'green': {'tags': {'landuse': 'grass', 'natural': ['island', 'wood'], 'leisure': 'park'}},
@@ -44,7 +44,7 @@ def create_map(radius: int = 500) -> io.BytesIO:
             'forest': {'fc': '#64B96A', 'ec': '#2F3737', 'lw': 1, 'zorder': 1},
             'water': {'fc': '#a1e3ff', 'ec': '#2F3737', 'hatch': 'ooo...', 'hatch_c': '#85c9e6', 'lw': 1, 'zorder': 2},
             'parking': {'fc': '#F2F4CB', 'ec': '#2F3737', 'lw': 1, 'zorder': 3},
-            'streets': {'fc': '#2F3737', 'ec': '#475657', 'alpha': 1, 'lw': 0, 'zorder': 3},
+            # 'streets': {'fc': '#2F3737', 'ec': '#475657', 'alpha': 1, 'lw': 0, 'zorder': 3},
             'building': {'palette': ['#FFC857', '#E9724C', '#C5283D'], 'ec': '#2F3737', 'lw': .5, 'zorder': 4},
         },
         osm_credit=False,
