26adbe04589880082da65302c0cd31c98cbff542
Adam Weeden <adam.weeden@nrccua.org>
2021-08-31 16:48:30 -0400

    Combine green/forests

--- api/maps.py
+++ api/maps.py
@@ -33,15 +33,15 @@ def create_map(radius: int = 500) -> io.BytesIO:
             # },
             'building': {'tags': {'building': True, 'landuse': 'construction'}, 'union': False},
             'water': {'tags': {'natural': ['water', 'bay']}},
-            'green': {'tags': {'landuse': 'grass', 'natural': ['island', 'wood'], 'leisure': 'park'}},
-            'forest': {'tags': {'landuse': 'forest'}},
+            'green': {'tags': {'landuse': ['grass', 'forest'], 'natural': ['island', 'wood'], 'leisure': 'park'}},
+            # 'forest': {'tags': {'landuse': 'forest'}},
             'parking': {'tags': {'amenity': 'parking', 'highway': 'pedestrian', 'man_made': 'pier'}}
         },
         drawing_kwargs={
             'background': {'fc': '#F2F4CB', 'ec': '#dadbc1', 'hatch': 'ooo...', 'zorder': -1},
             'perimeter': {'fc': '#F2F4CB', 'ec': '#dadbc1', 'lw': 0, 'hatch': 'ooo...',  'zorder': 0},
             'green': {'fc': '#D0F1BF', 'ec': '#2F3737', 'lw': 1, 'zorder': 1},
-            'forest': {'fc': '#64B96A', 'ec': '#2F3737', 'lw': 1, 'zorder': 1},
+            # 'forest': {'fc': '#64B96A', 'ec': '#2F3737', 'lw': 1, 'zorder': 1},
             'water': {'fc': '#a1e3ff', 'ec': '#2F3737', 'hatch': 'ooo...', 'hatch_c': '#85c9e6', 'lw': 1, 'zorder': 2},
             'parking': {'fc': '#F2F4CB', 'ec': '#2F3737', 'lw': 1, 'zorder': 3},
             # 'streets': {'fc': '#2F3737', 'ec': '#475657', 'alpha': 1, 'lw': 0, 'zorder': 3},
