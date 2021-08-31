1786387270fbf3a2cbdc0992747fb4f3e42bcf89
Adam Weeden <adam.weeden@nrccua.org>
2021-08-31 15:45:41 -0400

    Add CORS to api

--- api/main.py
+++ api/main.py
@@ -1,11 +1,21 @@
 from typing import Any, Dict
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from api import maps
 
 app = FastAPI()
 
+origins = ['https://thecleric.github.io']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def create_map(radius: int = 500) -> Dict[str, Any]:
